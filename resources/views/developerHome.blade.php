3626cabd72704aa1f13bcc9e8ad53c6eeb1165f3
Robin Yang <u1352883@unimail.hud.ac.uk>
2017-01-03 17:48:13 +0000

    Basic implementations in DB and Layouts

---
+++ resources/views/developerHome.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section ('header')
+
+@section('content')
+
+<div class="content">
+              <div class="title m-b-md">
+                  Developer Information
+              </div><br/>
+
+              <div class="links">
+                  <a>Robin Yang</a>
+                  <a>U1352883</a>
+                  <a>BSc Computing</a>
+                  <a>University of Huddersfield</a>
+              </div>
+            </div><br>
+            <a>View Website</a>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<title> Layout </title>
+
+<link rel="stylesheet" href="/css/style.css">
+
+    <head></head>
+
+    @yield('header')
+
+      <body>
+
+      @yield('content')
+
+      </body>
+
+
+</html>
